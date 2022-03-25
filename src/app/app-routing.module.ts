911a21f242f8afd465bcec20a86e4a7ff206ebf2
artemnih <23387542+artemnih@users.noreply.github.com>
2022-03-25 15:50:09 -0400

    feat: add apps via form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { DemoComponent } from './components/demo/demo.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [
+export const APP_ROUTES: Routes = [
   {
     path: '',
-    children: [
-      {
-        path: '',
-        component: DemoComponent,
-      },
-      {
-        path: 'mfe',
-        loadChildren: () =>
-          loadRemoteModule({
-            type: 'module',
-            remoteEntry: 'http://localhost:3000/remoteEntry.js',
-            exposedModule: './Module'
-          }).then(m => m.FlightsModule),
-      }
-    ]
+    component: DemoComponent,
+  },
+  {
+    path: 'mfe2',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:3000/remoteEntry.js',
+        exposedModule: './Module'
+      }).then(m => m.FlightsModule),
   }
 ];
 
-export const UsersRouting = RouterModule.forRoot(routes);
+export const UsersRouting = RouterModule.forRoot(APP_ROUTES);
--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,5 +1,9 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '@labshare/base-ui-services';
+import { APP_ROUTES } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-demo',
@@ -8,8 +12,16 @@ import { AuthService } from '@labshare/base-ui-services';
 })
 export class DemoComponent implements OnInit {
   private onAuthorizationResult;
+  public apps = [] as any[];
+  public form = new FormGroup({
+    name: new FormControl('demo'),
+    path: new FormControl('demo'),
+    remote: new FormControl('http://localhost:3000/remoteEntry.js'),
+    exposed: new FormControl('./Module'),
+    ngModule: new FormControl('FlightsModule'),
+  })
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router,) {
     this.onAuthorizationResult = this.authService.onAuthorizationResult();
   }
 
@@ -24,6 +36,28 @@ export class DemoComponent implements OnInit {
 
   auth() {
     this.authService.login();
+  }
+
+  addApp() {
+    const { name, path, remote, exposed, ngModule } = this.form.value;
+    const appRoute = {
+      name: name,
+      route: {
+        path: path,
+        loadChildren: () =>
+          loadRemoteModule({
+            type: 'module',
+            remoteEntry: remote,
+            exposedModule: exposed
+          }).then(m => m[ngModule]),
+      }
+    }
+
+    this.apps.push(appRoute);
+
+    const lazyRoutes = this.apps.map(app => app.route);
+    const routes = [...APP_ROUTES, ...lazyRoutes];
+    this.router.resetConfig(routes);
   }
 
 }
